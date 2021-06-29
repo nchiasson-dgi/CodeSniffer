1957efed8c5aa86272b619652872ff10b68486ab
Noel Chiasson <53783039+nchiasson-dgi@users.noreply.github.com>
2021-06-29 13:22:39 -0300

    Update lint.sh

--- lint.sh
+++ lint.sh
@@ -6,7 +6,7 @@ set -e #Exit entire script if any command fails
 echo "Downloading dependencies ..."
 composer -q global require drupal/coder
 composer -q global require dealerdirect/phpcodesniffer-composer-installer
-#curl -OLsS https://github.com/phpstan/phpstan/raw/master/phpstan.phar
+# curl -OLsS https://github.com/phpstan/phpstan/raw/master/phpstan.phar
 
 # Register sniffs
 echo "Registering sniffs ..."
@@ -26,5 +26,5 @@ echo "Running PHPCPD for copy/paste detection ..."
 ~/.composer/vendor/bin/phpcpd --names=*.php,*.module,*.inc,*.install,*.test,*.profile,*.theme,*.js,*.css,*.info --extensions=$ACTION_EXTENSIONS $ACTION_PATH
 
 # Throwing Reflection errors, need to specify core directories.
-#echo "Running PHPStan ..."
-#php ./phpstan.phar analyse $ACTION_PATH
+# echo "Running PHPStan ..."
+# php ./phpstan.phar analyse $ACTION_PATH
