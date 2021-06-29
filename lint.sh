52b8a2534170f14443286acdd7b77ef705f37db2
Noel Chiasson <53783039+nchiasson-dgi@users.noreply.github.com>
2021-06-29 12:57:53 -0300

    Update lint.sh

--- lint.sh
+++ lint.sh
@@ -12,12 +12,19 @@ curl -OLsS https://github.com/phpstan/phpstan/raw/master/phpstan.phar
 echo "Registering sniffs ..."
 ~/.composer/vendor/bin/phpcs -q --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer/
 
+echo "Running PHPCPD for Drupal version ..."
+~/.composer/vendor/bin/phpcpd --version
+
 # Run linting and static analysis
 echo "Running PHPCS for Drupal standards ..."
 ~/.composer/vendor/bin/phpcs --standard=Drupal --extensions=$ACTION_EXTENSIONS $ACTION_PATH
 
 echo "Running PHPCS for Drupal best practices ..."
 ~/.composer/vendor/bin/phpcs --standard=DrupalPractice --extensions=$ACTION_EXTENSIONS $ACTION_PATH
-  
-echo "Running PHPStan ..."
-php ./phpstan.phar analyse $ACTION_PATH
+
+echo "Running PHPCPD for copy/paste detection ..."
+~/.composer/vendor/bin/phpcpd --names=*.php,*.module,*.inc,*.install,*.test,*.profile,*.theme,*.js,*.css,*.info --extensions=$ACTION_EXTENSIONS $ACTION_PATH
+
+# Throwing Reflection errors, need to specify core directories.
+#echo "Running PHPStan ..."
+#php ./phpstan.phar analyse $ACTION_PATH
