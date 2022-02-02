33a880cb8dd23f918294d8cdae1d79cf45994a19
Noel Chiasson <noel@discoverygarden.ca>
2022-02-02 12:49:28 -0400

    Updating to coders new config location as of 8.3.14

--- lint.sh
+++ lint.sh
@@ -4,13 +4,13 @@ set -e #Exit entire script if any command fails
 
 # Download dependencies
 echo "Downloading dependencies ..."
-composer global require drupal/coder:8.3.13
+composer global require drupal/coder
 composer global require dealerdirect/phpcodesniffer-composer-installer
 composer global require sebastian/phpcpd
 
 # Register sniffs
 echo "Registering sniffs ..."
-~/.composer/vendor/bin/phpcs -q --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer/
+~/.composer/vendor/bin/phpcs -q --config-set installed_paths ~/.config/composer/vendor/drupal/coder/coder_sniffer/
 
 # Run linting and static analysis
 echo "Running PHPCS for Drupal standards ..."
